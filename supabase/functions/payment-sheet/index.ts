019f5eed65b0f93b2fa8a42f0515fd8da02fb336
DaveyEke <daveydenco@gmail.com>
2024-09-11 09:31:52 +0100

    Creating Stripe Payment Intent with supabase status

---
+++ supabase/functions/payment-sheet/index.ts
@@ -0,0 +1,72 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { stripe } from "../_utils/stripe.ts"
+
+console.log("Hello from Functions!")
+
+Deno.serve(async (req) => {
+ try {
+  const { amount } = await req.json()
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount : amount,
+    currency : 'USD'
+  })
+ 
+  const res = {
+   paymentIntent : paymentIntent.client_secret,
+   publishableKey : Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+  };
+ 
+  //  const data = {
+  //    message: `Hello  ${name}!`,
+  //  }
+ 
+   return new Response(
+     JSON.stringify(res),
+     { headers: { "Content-Type": "application/json" } },
+   )
+ } catch (error) {
+  console.log(error)
+  return new Response(JSON.stringify(error)), {
+    headers : { "Content-Type": "application/json" },
+    status : 400
+   }
+ }
+})
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Davey"}'
+
+    // Dave use this one
+    curl.exe -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' `
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' `
+    --header 'Content-Type: application/json' `
+    --data '{"name":"Davey"}'
+
+    // or you use this
+
+    Invoke-WebRequest -Uri 'http://127.0.0.1:54321/functions/v1/payment-sheet' `
+    -Method POST `
+    -Headers @{ 'Authorization' = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'; 'Content-Type' = 'application/json' } `
+    -Body '{"amount":1150}'
+
+    //  This also looks feasable
+
+    curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' 
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' `
+    --header 'Content-Type: application/json' `
+    --data '{"name":"Davey"}'
+
+*/
